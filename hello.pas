5f60c126e0d07d721568c8d090b9eaae6cb3a827
Юлия Петленко <juliaandreevnapetlenko@gmail.com>
2024-10-13 21:15:58 +0300

    Change file

--- hello.pas
+++ hello.pas
@@ -11,46 +11,46 @@ begin
   println('Введите номер языка на котором вы разговариваете:');
   println('Введите 1, если вы говорите на русском');
   println('Enter 2 if you speak English');
-  println('Ingrese 2 si habla Inglés');
-  println('如果你说英语，请输入2');
-  var n:=ReadInteger();
+  println('Ingrese 3 si habla Inglés');
+  println('如果你说英语，请输入4');
+  var n := ReadInteger();
   case n of
-    1:println('Привет!');
-    2:println('Hello!');
-    3:println('Hola!');
-    4:println('嗨。!');
+    1: println('Привет!');
+    2: println('Hello!');
+    3: println('Hola!');
+    4: println('嗨。!');
   end;
 end.
 {
 Введите номер языка на котором вы разговариваете:
 Введите 1, если вы говорите на русском
 Enter 2 if you speak English
-Ingrese 2 si habla Inglés
-如果你说英语，请输入2
+Ingrese 3 si habla Inglés
+如果你说英语，请输入4
 1
 Привет!
 
 Введите номер языка на котором вы разговариваете:
 Введите 1, если вы говорите на русском
 Enter 2 if you speak English
-Ingrese 2 si habla Inglés
-如果你说英语，请输入2
+Ingrese 3 si habla Inglés
+如果你说英语，请输入4
 2
 Hello!
 
 Введите номер языка на котором вы разговариваете:
 Введите 1, если вы говорите на русском
 Enter 2 if you speak English
-Ingrese 2 si habla Inglés
-如果你说英语，请输入2
+Ingrese 3 si habla Inglés
+如果你说英语，请输入4
 3
 Hola!
 
 Введите номер языка на котором вы разговариваете:
 Введите 1, если вы говорите на русском
 Enter 2 if you speak English
-Ingrese 2 si habla Inglés
-如果你说英语，请输入2
+Ingrese 3 si habla Inglés
+如果你说英语，请输入4
 4
 嗨。!
 
